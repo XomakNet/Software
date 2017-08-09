c56ee7e6f4397717d5376b6660ebfe235e1efc46
isenilov <isenilov@gmail.com>
2017-08-09 16:55:01 +0300

    Removed import of AprilTags

--- catkin_ws/src/f23-LED/led_interpreter/src/LED_interpreter_node.py
+++ catkin_ws/src/f23-LED/led_interpreter/src/LED_interpreter_node.py
@@ -3,7 +3,7 @@ import rospy
 import time
 #from led_detection.LEDDetector import LEDDetector
 from std_msgs.msg import Byte
-from duckietown_msgs.msg import FSMState, Vector2D, AprilTags, LEDDetection, LEDDetectionArray, LEDDetectionDebugInfo, SignalsDetection 
+from duckietown_msgs.msg import FSMState, Vector2D, LEDDetection, LEDDetectionArray, LEDDetectionDebugInfo, SignalsDetection #, AprilTags
 from sensor_msgs.msg import CompressedImage
 #from duckietown_utils.bag_logs import numpy_from_ros_compressed
 #import numpy as np
