fdc461cc74ba73637d5ba24172f5214be4292e50
Xomak <mail@xomak.net>
2017-10-08 19:57:57 +0300

    Change setup-related files

--- ament_ws/05-teleop/adafruit_drivers/setup.py
+++ ament_ws/05-teleop/adafruit_drivers/setup.py
@@ -1,12 +1,12 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup, find_packages
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['Adafruit_ADS1x15', 'Adafruit_GPIO','Adafruit_I2C','Adafruit_LSM303','Adafruit_MotorHAT','Adafruit_PWM_Servo_Driver','Gyro_L3GD20'],
-    package_dir={'': 'include'},
-)
+setup_args = {
+    "name": "adafruit_drivers_py",
+    "packages": ['Adafruit_ADS1x15', 'Adafruit_GPIO','Adafruit_I2C','Adafruit_LSM303','Adafruit_MotorHAT','Adafruit_PWM_Servo_Driver','Gyro_L3GD20'],
+    "package_dir": {'': 'include'},
+}
 
 setup(**setup_args)
