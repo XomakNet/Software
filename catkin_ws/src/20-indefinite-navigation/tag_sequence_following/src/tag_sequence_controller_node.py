9d9e09a1e4a8172d53782d42eeb18de8a42e3136
Xomak <mail@xomak.net>
2017-12-02 16:52:16 +0300

    Fix bugs in tag_sequence_following

--- catkin_ws/src/20-indefinite-navigation/tag_sequence_following/src/tag_sequence_controller_node.py
+++ catkin_ws/src/20-indefinite-navigation/tag_sequence_following/src/tag_sequence_controller_node.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import rospy
-from duckietown_msgs.msg import BoolStamped, AprilTagDetectionArray, FSMState
+from duckietown_msgs.msg import AprilTagDetectionArray, FSMState
 from duckietown_msgs.srv import SetTagsSequence, GetCurrentAction
 from std_srvs.srv import Empty, EmptyResponse
+from std_msgs.msg import Bool
 
 
 __author__ = "Xomak"
@@ -13,11 +14,14 @@ class TagSequenceControllerNode(object):
     def __init__(self):
         self.node_name = rospy.get_name()
 
+        self.tag_available_topic = rospy.Subscriber('~tag_available', Bool, self.on_tag_available)
         self.fsm_state_topic = rospy.Subscriber('~fsm_state', FSMState, self.on_fsm_state)
         self.intersection_state = rospy.get_param('~intersection_state')
         self.turn_service_prefix = rospy.get_param('~turn_service_prefix', '~turn_')
 
         self.get_current_action = rospy.ServiceProxy('~get_current_action', GetCurrentAction)
+
+        self.waiting_for_tag = False
 
         rospy.Service("~init_movement", Empty, self.on_init_movement)
 
@@ -25,7 +29,7 @@ class TagSequenceControllerNode(object):
 
     def on_init_movement(self, req):
         self.turn()
-        return EmptyResponse()
+        return {}
 
     def turn(self):
         rospy.loginfo("Requesting action info...")
@@ -35,10 +39,22 @@ class TagSequenceControllerNode(object):
             rospy.loginfo("Will perform: %s" % action_info.action)
             turn = rospy.ServiceProxy(self.turn_service_prefix + action_info.action, Empty)
             turn()
+        elif action_info.no_tag:
+            rospy.loginfo("No tag available, waiting...")
+            self.waiting_for_tag = True
+
+    def on_tag_available(self, msg):
+        if self.waiting_for_tag and msg.data:
+            rospy.loginfo("Tag available")
+            self.waiting_for_tag = False
+            self.turn()
 
     def on_fsm_state(self, msg):
         if msg.state == self.intersection_state:
+            rospy.loginfo("FSM changed to intersection state")
             self.turn()
+        else:
+            self.waiting_for_tag = False
 
     def on_shutdown(self):
         rospy.loginfo("[%s] Shutting down." % self.node_name)
--- catkin_ws/src/20-indefinite-navigation/tag_sequence_following/src/tag_sequence_detector_node.py
+++ catkin_ws/src/20-indefinite-navigation/tag_sequence_following/src/tag_sequence_detector_node.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 import rospy
-from duckietown_msgs.msg import BoolStamped, AprilTagDetectionArray
-from duckietown_msgs.srv import SetTagsSequence, GetCurrentAction
-from std_srvs.srv import EmptyResponse
 import tf.transformations as tr
-
+from duckietown_msgs.msg import AprilTagDetectionArray
+from duckietown_msgs.srv import SetTagsSequence, GetCurrentAction
+from std_msgs.msg import Bool
 
 __author__ = "Xomak"
 
 
 class TagSequenceDetectorNode(object):
-
     def __init__(self):
         self.node_name = rospy.get_name()
 
         self.tags_topic = rospy.Subscriber('~tags', AprilTagDetectionArray, self.on_tag_detections)
+        self.tag_available = rospy.Publisher('~tag_available', Bool)
 
         self.current_sequence = None
         self.current_index = None
@@ -29,7 +28,7 @@ class TagSequenceDetectorNode(object):
     def on_sequence(self, req):
         self.current_sequence = req.sequence
         self.current_index = 0
-        return EmptyResponse()
+        return {}
 
     def find_tag_id_in_sequence(self, tag_id):
         for idx, action in enumerate(self.current_sequence):
@@ -83,22 +82,25 @@ class TagSequenceDetectorNode(object):
 
     def on_tag_detections(self, msg):
         tags = msg.detections
-
+        tags = [tag for tag in tags if tag.pose.pose.position.x > 0]
         if len(tags) > 0:
             tags_with_angles = []
             for tag in tags:
                 orientation = tag.pose.pose.orientation
+
                 euler_angles = tr.euler_from_quaternion([orientation.x,
-                                                  orientation.y,
-                                                  orientation.z,
-                                                  orientation.w])
+                                                         orientation.y,
+                                                         orientation.z,
+                                                         orientation.w])
                 tags_with_angles.append((euler_angles[0], tag))
 
             tags_with_angles.sort(key=lambda x: x[0])
             main_tag = tags_with_angles[0][1]
             self.last_detected_tag_id = main_tag.id
+            self.tag_available.publish(Bool(True))
         else:
             self.last_detected_tag_id = None
+            self.tag_available.publish(Bool(False))
 
     def on_shutdown(self):
         rospy.loginfo("[%s] Shutting down." % self.node_name)
